ca86f8bbfc068bf4f9e0c79dc86e3ceff93b47e8
rellyman53 <65138789+rellyman53@users.noreply.github.com>
2022-04-25 21:22:39 -0500

    command framework set up

--- arc/addmarket.js
+++ arc/addmarket.js
@@ -3,6 +3,8 @@ const adminRole = "968192732122210364"
 const staffRole = "968191734653808692"
 const collectorRole = "968191744246177812"
 
+const channel = ""
+
 module.exports.run = async (client, message, args) => {
     if(message.member.roles.cache.has(ownerRole) || message.member.roles.cache.has(adminRole) || message.member.roles.cache.has(staffRole) || message.member.roles.cache.has(collectorRole) || message.author.id === "312734500880646145") {
         message.reply(`this works`)
